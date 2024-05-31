4f73b79dc1babd42303f96fde24f33afacf712cb
Mills <39015355+tiredandsad@users.noreply.github.com>
2024-05-31 10:06:47 -0600

    Initial code push

---
+++ Script.js
@@ -0,0 +1,8 @@
+//I have included document pointers for the outputText and the userResponse.
+//You need to figure out how to take the user's response and then update the 
+//output text to prompt them again. If you want to get more advanced I 
+//recommend trying to change the prompt the user gets, resetting the text 
+//in the input area when the user presses enter, and other things you want.
+
+const outputText = document.querySelector(".outputText");
+const userResponse = document.querySelector(".userResponse");
